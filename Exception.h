e51a7ed35962e370f5a7b461e3527eefec940214
Abdul Yahya <auxkord@users.noreply.github.com>
2015-10-10 10:35:08 -0700

    Updated Declarations

--- Exception.h
+++ Exception.h
@@ -1,7 +1,7 @@
 /***********************************************************
 * Author:				Abdul Yahya
 * Created:				10/04/15
-* Updated:				10/04/15
+* Updated:				10/10/15
 * H.W. Number:			CST 211 Assignment 2
 * Filename:				Exception.h
 ************************************************************/
@@ -14,18 +14,18 @@
 class cException
 {
 public:
-	cException ();
-	cException (char *msg);
-	cException (const cException &copy);
+	cException  ();
+	cException  ( char *msg );
+	cException  ( const cException &c );
 	~cException ();
 
-	cException &operator= (const cException &rhs);
+	cException &operator= ( const cException &rhs );
 
-	friend std::ostream &operator<< (std::ostream &stream, const cException &e);
+	friend std::ostream &operator<< ( std::ostream &stream, const cException &e );
 
-	const char* getMessage();
+	const char* getMessage ();
 
-	void setMessage(char *msg);
+	void setMessage ( char *msg );
 
 private:
 	char *m_msg;
