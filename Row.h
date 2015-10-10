662e1d504dafee033951bea2485ce2784775afda
Abdul Yahya <auxkord@users.noreply.github.com>
2015-10-10 10:35:37 -0700

    Updated cRow Declarations

--- Row.h
+++ Row.h
@@ -1,7 +1,7 @@
 /***********************************************************
 * Author:				Abdul Yahya
 * Created:				10/04/15
-* Updated:				10/04/15
+* Updated:				10/10/15
 * H.W. Number:			CST 211 Assignment 2
 * Filename:				Row.h
 ************************************************************/
@@ -13,7 +13,7 @@
 * Class: cRow
 *
 * Constructors:
-*	cRow ( cArray2D &array, int row );
+*	cRow ( cArray2D<T> &array, int row );
 *		Array and Row are initialized to 0.
 *************************************************************************/
 template <typename T> class cArray2D;
@@ -28,9 +28,9 @@ public:
 	T		&operator[] ( int column );			// Const Subscript Operator Constructor
 
 private:
-	cArray2D<T> &m_array2D;	// Placeholder
+	cArray2D<T> &m_array2D;	// Reference to cArray2D's m_array Array
 
-	int	m_row;				// PlaceHolder
+	int	m_row;				// Member variable used to store row values
 
 };
 
