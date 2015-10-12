7cfcbb2383cb54478145598694e834c5f59c2e0e
Abdul Yahya <abdullah.yahya@oit.edu>
2015-10-12 00:34:10 -0700

    Updated Definitions

--- Exception.h
+++ Exception.h
@@ -9,23 +9,28 @@
 #ifndef Exception_H
 #define Exception_H
 
+#define ERR_MEM_ALLOC 1
+
 #include <iostream>
 
 class cException
 {
 public:
 	cException  ();
-	cException  ( char msg );
+	cException  ( char *msg );
 	cException  ( const cException &c );
 	~cException ();
 
 	cException &operator= ( const cException &rhs );
 
-	friend std::ostream &operator<< ( std::ostream &stream, const cException &e );
+	friend std::ostream &operator<< (std::ostream &stream, const cException &e) 
+	{
+		return stream << e.m_msg;
+	}
 
-	const char* getMessage ();
+	const char* GetMessage () const;
 
-	void setMessage ( char *msg );
+	void SetMessage ( char *msg );
 
 private:
 	char *m_msg;
