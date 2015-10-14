938a1a10ebacceecc44858d216426c75fd3aa5f5
Abdul Yahya <Auxkord@users.noreply.github.com>
2015-10-14 00:05:02 -0700

    Comment Update

--- Exception.h
+++ Exception.h
@@ -1,7 +1,7 @@
 /***********************************************************
 * Author:				Abdul Yahya
 * Created:				10/04/15
-* Updated:				10/10/15
+* Updated:				10/13/15
 * H.W. Number:			CST 211 Assignment 2
 * Filename:				Exception.h
 ************************************************************/
@@ -9,9 +9,29 @@
 #ifndef Exception_H
 #define Exception_H
 
-#define ERR_MEM_ALLOC 1
-
 #include <iostream>
+#include <string.h>
+
+
+/************************************************************************
+* Class: cException
+*
+* Constructors:
+*	cException ()
+*		Array, Rows, and Columns are initialized to 0.
+*	cException( char *msg )
+*		Overloading char pointer msg.
+*	cException	( const cException &c )
+*		Copy Constructor.
+*
+* Mutators:
+*	void SetMessage ( char *msg )
+*		This mutator copies the string msg from the char pointer msg.
+*
+* Methods:
+*	int GetMessage ()
+*		This method gets the Message values.
+*************************************************************************/
 
 class cException
 {
@@ -25,7 +45,7 @@ public:
 
 	friend std::ostream &operator<< (std::ostream &stream, const cException &e) 
 	{
-		return stream << "Handled Exception: " << e.m_msg;
+		return stream << e.m_msg;
 	}
 
 	const char* GetMessage () const;
