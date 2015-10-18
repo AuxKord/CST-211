53707e8c86442ca4ba8e24f32ce118930ac7e6d5
AuxKord <abdullah.yahya@oit.edu>
2015-10-18 03:26:27 -0700

    Updated Template Class Array2D Constructors and Functions

--- Array2D.h
+++ Array2D.h
@@ -1,75 +1,96 @@
-/***********************************************************
+/**********************************************************************
 * Author:				Abdul Yahya
-* Created:				10/04/15
-* Updated:				10/13/15
-* H.W. Number:			CST 211 Assignment 2
+* Created:				10/16/15
+* Updated:				10/16/15
+* H.W. Number:			Assignment 2
 * Filename:				Array2D.h
-************************************************************/
+************************************************************************/
 
 #ifndef Array2D_H
 #define Array2D_H
 
+#include <iostream>
+
 #include "Row.h"
-#include "Exception.h"
 
 /************************************************************************
 * Class: cArray2D
 *
+* Purpose:  Create a row-major 2D Array of rows and columns. 
+*
 * Constructors:
 *	cArray2D ()
 *		Array, Rows, and Columns are initialized to 0.
-*	cArray2D ( int row, int col = 0 )
-*		Overloading Row and Col member Variables.
+*	cArray2D ( int row, int col )
+*		Array, Rows, and Columns are initialized with data.
 *	Array2D	( const Array2D &c )
-*		Copy Constructor.
+*		Copy Constructor
+*
+* Destructor:
+*	~cArray2D ()
+*		De-allocates the Array and sets it to zero.
+*
+* Operators:
+*	&operator= ( const cArray2D &array )
+*		Overloaded Assignment Operator de-allocates m_array
+*		if it contains old values. Allocates new values into tempArray
+*		and resizes the array accordingly. Deep copies tempArray values
+*		into m_array.
+*	const cRow<T> operator[] ( int index ) const
+*		Takes in the Array's index and returns the const RowObject
+*		value.
+*	cRow<T>		  operator[] ( int index )
+*		Takes in the Array's index and returns the non-Const RowObject
+*		value.
 *
 * Mutators:
-*	void SetRow ( int rows )
-*		This mutator initializes the data members to the specified
-*		parameters. It also ensures that the data is valid.
-*	void SetCol ( int columns )
-*		This mutator initializes the data members to the specified
-*		parameters. It also ensures that the data is valid.
+*	void SetRow ( int row )
+*		This mutator initializes the data member row to the specified
+*		parameter. It also ensures that the data is valid.
+*	void SetCol ( int col )
+*		This mutator initializes the data member col to the specified
+*		parameter. It also ensures that the data is valid.
 *
 * Methods:
-*	int GetRow ()
-*		This method gets the row values.
-*	int GetCol()
-*		This method gets the column values.
-*************************************************************************/
+*	int GetRow () const
+*		This method gets the const row values.
+*	int GetCol () const
+*		This method gets the const column values.
+************************************************************************/
 
 template <typename T>
 class cArray2D
 {
 public:
-	cArray2D  ();									// Default Constructor
-	cArray2D  ( int row, int col = 0 );				// Overloaded Row & Col constructor
-	cArray2D  ( const cArray2D &c );				// Copy Construtor
-	~cArray2D ();									// Destructor
+	cArray2D  ();								// Default Constructor
+	cArray2D  ( int row, int col );				// OVerloaded Default Constructor
+	cArray2D  ( const cArray2D<T> &array );		// Copy Constructor
+	~cArray2D ();								// Destructor
 
-	cArray2D &operator= ( const cArray2D &rhs );	// Overloaded Assignment Operator Constructor
+	cArray2D &operator= ( const cArray2D &array );	// Overloaded Assignment Operator
 
-	const cRow<T> operator[]  ( int index ) const;	// Array Index Operator Constructor
-	cRow<T>		  operator[]  ( int index );		// Array Index Operator Constructor
+	const cRow<T> operator[] ( int index ) const;	// Overloaded Const Subscript Operator
+	cRow<T>		  operator[] ( int index );			// Overloaded non-Const Subscript Operator
 
-	int GetRow ();	// Gets the Row Values
-	int GetCol ();	// Gets the Col Values
+	int GetRow () const;	// Method returns the private member variable m_row
+	int GetCol () const; 	// Method returns the private member variable m_col	
 
-	void SetRow ( int row );		// Checks and sets the Row Values
-	void SetCol ( int column );		// Checks and sets the Col Values
+	void SetRow ( int row );	// Mutator resizes the Array's row values
+	void SetCol ( int col );	// Mutator resizes the Array's column values
+	
+	void Resize ( int srow, int scol ); // Mutator Resize function for the Setters above
+	//void SetSize(int nrows, int ncols);
+	const T &Select ( int row, int col ) const;		// Overloaded Const Array Accessor
+	T		&Select ( int row, int col );			// Overloaded non-Const Array Accessor
 
-	T &Select ( int row, int column );	// Accesses row and column values
+private:
+	T** m_array;	// Data type 'T' pointer to pointer Array private member variable
 
-private: 
-	T** m_array;	// Pointer to Pointer Multi-Dimensional Array 
+	int m_row;		// Private member variable m_row stores row values
+	int m_col;		// Priavte member variable m_col stores column values
 
-	int m_row;		// Member variable used to store row values
-	int m_col;		// Member variable used to store column values
-
+	void ValidateIndex ( int row, int col = 0 ) const;	// Private void function validates the Array's row and column values
 };
 
 #include "Array2D.inc"
-
 #endif
-
-
